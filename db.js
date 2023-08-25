e48bcb389a23b952ef8485d944a1cb4aef444f76
Kevinen04 <139803867+Kevinen04@users.noreply.github.com>
2023-08-25 18:02:35 +0400

    first commit

---
+++ db.js
@@ -0,0 +1,11 @@
+const restaurants = require("./data/restaurants");
+const users = require("./data/users");
+const orders = require("./data/orders");
+const store = require("./data/store");
+
+module.exports = {
+    restaurants,
+    users,
+    orders,
+    store
+}
